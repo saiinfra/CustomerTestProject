57f8b484e39ff8e500d10dd82f7f532ae2bad9b6
saiinfra <skrishna@infrascape.com>
2015-12-09 16:52:10 +0530

    initial vesion

---
+++ src/com/test/TestCasesMap.java
@@ -0,0 +1,22 @@
+package com.test;
+
+import java.util.HashMap;
+
+public class TestCasesMap {
+
+	public HashMap<String, String> map= new HashMap<String, String>();
+	
+	public TestCasesMap(){
+		super();
+		map.put("SampleAddTest", "com.test.SampleAddTest");
+		map.put("TestJunit", "com.test.TestJunit");
+		map.put("SFReleaseActive", "com.test.SFReleaseActive");
+		map.put("SalesforceLoginJava", "com.test.SalesforceLoginJava");
+		map.put("SeleniumTest", "com.test.SeleniumTest");
+		map.put("RunSalesForceTestSuite", "com.test.RunSalesForceTestSuite");
+	}
+	
+	public HashMap<String, String> getMap(){
+		return map;
+	}
+}
