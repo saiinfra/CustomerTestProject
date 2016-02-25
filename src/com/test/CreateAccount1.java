eec401e84e20a9db131bc512d877e18fb21a21fa
saiinfra <skrishna@infrascape.com>
2016-02-25 15:46:13 +0530

    Update CreateAccount1.java

--- src/com/test/CreateAccount1.java
+++ src/com/test/CreateAccount1.java
@@ -1,46 +1,73 @@
 package com.test;
 
 import java.util.concurrent.TimeUnit;
-import org.junit.Test;
-import org.junit.Before;
-import org.junit.After;
-import org.openqa.selenium.*;
-import org.openqa.selenium.firefox.*;
 
+import junit.framework.Assert;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TestName;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class CreateAccount1 {
 	private WebDriver driver;
 	private String baseUrl;
 	private boolean acceptNextAlert = true;
 	private StringBuffer verificationErrors = new StringBuffer();
+	@Rule
+	public TestName name = new TestName();
 
-@Before
-public  void setUp() throws Exception {
-	driver = new FirefoxDriver();
+	@Before
+	public void setUp() throws Exception {
+		driver = new FirefoxDriver();
 
-	driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 	}
 
 	@Test
-	public voidTS_000000073() throws Exception{
-		driver.get("https://login.salesforce.com/"  );
+	public void TS_000000073() throws Exception {
+		System.out.println(name.getMethodName());
+
+		driver.get("https://login.salesforce.com/");
 		driver.findElement(By.id("username")).clear();
 		driver.findElement(By.id("username")).sendKeys("anand@DeployerDev.com");
 		driver.findElement(By.id("password")).clear();
 		driver.findElement(By.id("password")).sendKeys("Welcome123");
 		driver.findElement(By.id("Login")).click();
+
 	}
 
 	@Test
-	public voidTS_000000074() throws Exception{
+	public void TS_000000074() throws Exception {
+		System.out.println(name.getMethodName());
+
+		driver.findElement(By.id("username")).clear();
+		driver.findElement(By.id("username")).sendKeys("anand@DeployerDev.com");
+
 	}
 
 	@Test
-	public voidTS_000000075() throws Exception{
+	public void TS_000000075() throws Exception {
+		System.out.println(name.getMethodName());
+
+		driver.findElement(By.id("username")).clear();
+		driver.findElement(By.id("username")).sendKeys("anand@DeployerDev.com");
+		System.out.println(name.getMethodName());
+
 	}
 
 	@Test
-	public voidTS_000000076() throws Exception{
+	public void TS_000000076() throws Exception {
+		System.out.println(name.getMethodName());
+
+		driver.findElement(By.id("username")).clear();
+		driver.findElement(By.id("username")).sendKeys("anand@DeployerDev.com");
+		System.out.println(name.getMethodName());
+
 	}
 
 	@After
